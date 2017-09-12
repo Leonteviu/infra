0638dc684e30cd2b2599c344f7a632bebb0239f2
Igor Leontev <leonteviu@gmail.com>
2017-09-12 17:53:19 +0300

    Add FIREWALL rule to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,4 +23,14 @@ network = "default"
 access_config {}
 }
 }
+resource "google_compute_firewall" "firewall_puma" {
+name = "allow-puma-default"
+network = "default"
+allow {
+protocol = "tcp"
+ports = ["9292"]
+}
+source_ranges = ["0.0.0.0/0"]
+target_tags = ["reddit-app"]
+}
 
