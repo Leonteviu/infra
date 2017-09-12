2aeabf9775cad720fbefe9feb112f01cef4a0a8c
Igor Leontev <leonteviu@gmail.com>
2017-09-12 17:34:33 +0300

    Set resource in main.tf to create VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,22 @@ provider "google" {
 project = "infra-179710"
 region = "europe-west1"
 }
+resource "google_compute_instance" "app" {
+name = "reddit-app"
+machine_type = "g1-small"
+zone = "europe-west1-b"
+# BOOT DISK
+boot_disk {
+initialize_params {
+image = "reddit-base-1505214895"
+}
+}
+# NETWORK INTERFACE
+network_interface {
+# NET to Connect
+network = "default"
+# Use ephemeral IP to connect from Internet
+access_config {}
+}
+}
+
