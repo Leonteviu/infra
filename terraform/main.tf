e365dadbacd104d5124f963db860408c99942453
Igor Leontev <leonteviu@gmail.com>
2017-09-15 15:16:45 +0300

    Add SSH firewall rule to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,3 +65,14 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_ssh" {
+name = "default-allow-ssh"
+network = "default"
+description = "Allow SSH from all hosts"
+allow {
+protocol = "tcp"
+ports = ["22"]
+}
+source_ranges = ["0.0.0.0/0"]
+}
